286c3236f293e4dc78851782bdd4b7a6212304a2
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2016-08-06 23:29:42 +0200

    Add port number
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class rocketchat (
   $root_url       = $::rocketchat::root_url,
   $mongo_url      = $::rocketchat::mongo_url,
   $mail_url       = $::rocketchat::mail_url,
-  $manage_service = $::rocketchat::manage_service,
+  $port           = $::rocketchat::port,
   $package        = $::rocketchat::package,
 ) inherits rocketchat::params {
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class rocketchat::params {
   $root_url       = 'http://your-host-name.com-as-accessed-from-internet:3000/'
   $mongo_url      = 'mongodb://localhost:27017/rocketchat'
   $mail_url       = 'smtp://yourmailbox@yourdomain.com:yourpassword@smtp.yourmailserver.com:465/'
-  $manage_service = true
+  $port           = 3000
   $package        = 'rocketchat'
 
 }
